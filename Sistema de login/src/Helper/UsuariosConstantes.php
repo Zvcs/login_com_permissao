fab75dee3240f0249338862b8af7df5c49ec169d
José Victor <67513012+Zvcs@users.noreply.github.com>
2023-02-28 06:50:44 -0300

    Add files via upload

---
+++ Sistema de login/src/Helper/UsuariosConstantes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace src\login\constantes;
+
+class ConstantesLogin
+{
+    public const ADMIN = 'Admin';
+    public const ESCRITOR = 'Escritor';
+    public const LEITOR = 'Leitor';
+}
